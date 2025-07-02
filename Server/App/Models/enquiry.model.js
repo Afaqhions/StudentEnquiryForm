95ede322b6dbac86154b96529ca87148ba6f57e2
Afaqhions <m.afaqpak@gmail.com>
2025-07-02 14:24:31 +0500

    up

---
+++ Server/App/Models/enquiry.model.js
@@ -0,0 +1,26 @@
+let mongoose = require("mongoose");
+
+// Creating Schema for Enquiry
+let enquirySchema = new mongoose.Schema({
+    name:{type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    message:{
+        type:String,
+        requires:true
+    }
+})
+// Packing Model to Enquiry
+let Enquiry = mongoose.model("Enquiry", enquirySchema);
+
+// Exporting Enquiry Model
+module.exports = Enquiry;
---
+++ Server/index.js
@@ -0,0 +1,31 @@
+// Importing express
+let express = require("express");
+
+// Importing Mongoose
+let mongoose = require("mongoose");
+
+// Configuring dotenv
+require("dotenv").config();
+
+// Initilaizing Express to App
+let app = express();
+
+// Us json Middleware
+app.use(express.json());
+
+// Importing Client API
+let clientApi = require("./App/routes/Client/clientApi");
+// Routes
+app.use("/api/client",clientApi)
+
+
+// Connecting to mongoDB
+mongoose.connect(process.env.DB_url).then(()=>{
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server Running.");
+        
+    })
+}).catch((err)=>{
+    console.log("Error connecting to MongoDB: ", err);
+})
